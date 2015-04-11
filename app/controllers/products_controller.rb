cbb796fb1977cae8cc96fedfdcd6e639b34727f4
dwayhs <danielwayhs@gmail.com>
2015-04-11 14:58:09 -0300

    produto: adicionado vínculo com usuário
     - verificação da autenticação na criação/edição de produto
     - vinculo no create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /products
   # GET /products.json
@@ -25,6 +26,7 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
+    @product.user = current_user
 
     respond_to do |format|
       if @product.save
