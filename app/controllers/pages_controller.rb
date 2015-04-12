e9f17f75b31eac7b0dd1c3fdd465c24d59ab6c79
Gerson Azevedo <gerson@ayabrasil.info>
2015-04-11 22:00:02 -0300

    mais estilos adicionados. criada tela de produto

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
 	def home
 
 	end
+
+	def produto
+
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
 
   get "pages/home", to: "pages#home"
+  get "pages/produto", to: "pages#produto"
 
   devise_for :users
 
