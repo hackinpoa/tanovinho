1ef11987402164e675b9b0cb9b98b48488ab7521
Marlon Bernardes <marlonjb.cb@gmail.com>
2015-04-12 02:04:42 -0300

    chore: adicionar ap response

--- app/services/pag_seguro_service.rb
+++ app/services/pag_seguro_service.rb
@@ -7,6 +7,7 @@ class PagSeguroService
     Rails.logger.info "Iniciando consulta de notificaçoes de transação..."
     wsUrl = "https://ws.pagseguro.uol.com.br/v3/transactions/notifications/#{code}?token=#{@@token}&email=#{@@email}"
     response = HTTParty.get(wsUrl);
+    ap response
     Rails.logger.info %Q("
         [reference: #{response.transaction.reference}]
         [status: #{response.transaction.status}]
