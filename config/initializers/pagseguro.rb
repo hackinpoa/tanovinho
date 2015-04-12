85aa92c18259fb26b39c7d7e642d6fe11ffe39e5
dwayhs <danielwayhs@gmail.com>
2015-04-12 03:30:09 -0300

    correcoes na visualizacao de produtos

--- config/initializers/pagseguro.rb
+++
@@ -1,6 +0,0 @@
-raise "Variável de ambiente PAGSEGURO_EMAIL não informada" if ENV["PAGSEGURO_EMAIL"].nil?
-raise "Variável de ambiente PAGSEGURO_TOKEN não informada" if ENV["PAGSEGURO_TOKEN"].nil?
-raise "Variável de ambiente PAGSEGURO_PRODUCTION não informada" if ENV["PAGSEGURO_PRODUCTION"].nil?
-
-puts "Usando sandbox no PagSeguro" if ENV["PAGSEGURO_PRODUCTION"] == "false"
-PagSeguro.environment = :sandbox if ENV["PAGSEGURO_PRODUCTION"] == "false"
