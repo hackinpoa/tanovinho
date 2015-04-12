0394b32196905d65e89e6d32e7d73e733888dfdc
dwayhs <danielwayhs@gmail.com>
2015-04-12 06:11:01 -0300

    implementada paginação na página de produtos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.filter(product_filter_params)
+    @products = Product.filter(product_filter_params).page(params[:page])
   end
 
   # GET /products/1
